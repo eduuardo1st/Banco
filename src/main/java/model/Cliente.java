2d2a05cf78d0e81e39fe1482525430dbcb2224a9
Eduardo Gonçalves <eduardouc388@gmail.com>
2025-11-11 20:53:44 -0300

    Pacote model implementado

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -1,4 +1,29 @@
 package model;
 
 public class Cliente {
+    private String nome;
+    private String cpf;
+    private Conta conta; // Agregação
+
+    public Cliente(String nome, String cpf, String numeroConta, double limiteConta) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.conta = new Conta(numeroConta, 0.0, limiteConta);
+    }
+
+    public boolean realizarDeposito(double valor) {
+        return this.conta.depositar(valor);
+    }
+
+    public boolean realizarSaque(double valor) {
+        return this.conta.sacar(valor);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Conta getConta() {
+        return conta;
+    }
 }
--- src/main/java/model/Conta.java
+++ src/main/java/model/Conta.java
@@ -1,4 +1,41 @@
 package model;
 
 public class Conta {
+    private String numero;
+    private double saldo;
+    private double limite;
+
+    public Conta(String numero, double saldoInicial, double limite) {
+        this.numero = numero;
+        this.saldo = saldoInicial;
+        this.limite = limite;
+    }
+
+    public boolean sacar(double valor) {
+        if (valor > 0 && (this.saldo + this.limite) >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean depositar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            return true;
+        }
+        return false;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
 }
